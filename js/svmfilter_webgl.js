d2919207bc52135bf2f706fd4964281e464bac13
Shunichi Otsuka <vascoosx@gmail.com>
2014-05-25 09:09:24 +0900

    correct triangle height calculations previously the "y" offset was not being calculated so this was added. Also added interspace to avoid mixing border pixel values from surrounding patches.

--- js/svmfilter_webgl.js
+++ js/svmfilter_webgl.js
@@ -389,38 +389,43 @@ var webglFilter = function() {
     irectangles = new Float32Array(irectangles);
 
     if ('lbp' in filters || 'sobel' in filters) {
+      var topCoord = 1.0 - 2/(patchHeight*numBlocks);
+      var bottomCoord = 1.0 - 2/numBlocks + 2/(patchHeight*numBlocks);
+      var yOffset;
       // calculate position of vertex rectangles for gradient/lbp program
       var gradRectangles = [];
       for (var i = 0;i < numBlocks;i++) {
         //first triangle
         gradRectangles = gradRectangles.concat(
-          [-1.0, 1.0, 
-          1.0, 1.0,
-          -1.0, -1.0]
+          [-1.0, topCoord - yOffset, 
+          1.0, topCoord - yOffset,
+          -1.0, bottomCoord - yOffset]
         );
         //second triangle
         gradRectangles = gradRectangles.concat(
-          [-1.0, -1.0, 
-          1.0, 1.0,
-          1.0, -1.0]
+          [-1.0, bottomCoord - yOffset, 
+          1.0, topCoord - yOffset,
+          1.0, bottomCoord - yOffset]
         );
       }
       gradRectangles = new Float32Array(gradRectangles);
       
+      topCoord = 1.0 - 1/(patchHeight*numBlocks);
+      bottomCoord = 1.0 - 1/numBlocks + 1/(patchHeight*numBlocks);
       // calculate position of image rectangles to draw out
       var gradIRectangles = [];
       for (var i = 0;i < numBlocks;i++) {
         //first triangle
         gradIRectangles = gradIRectangles.concat(
-          [0.0, 1.0, 
-          1.0, 1.0,
-          0.0, 0.0]
+          [0.0, topCoord - yOffset, 
+          1.0, topCoord - yOffset,
+          0.0, bottomCoord - yOffset]
         );
         //second triangle
         gradIRectangles = gradIRectangles.concat(
-          [0.0, 0.0, 
-          1.0, 1.0,
-          1.0, 0.0]
+          [0.0, bottomCoord - yOffset, 
+          1.0, topCoord - yOffset,
+          1.0, bottomCoord - yOffset]
         );
       }
       gradIRectangles = new Float32Array(gradIRectangles);
