366a16cb9cc3db8b0b160a237d6ce7ab9f1593fd
Audun Mathias Øygard <amoygard@gmail.com>
2017-07-09 00:43:26 +0200

    use webm video instead of ogv in examples

--- examples/js/libs/utils.js
+++ examples/js/libs/utils.js
@@ -37,8 +37,8 @@ function supports_h264_baseline_video() {
   return v.canPlayType('video/mp4; codecs="avc1.42E01E, mp4a.40.2"');
 }
 
-function supports_ogg_theora_video() {
+function supports_webm_video() {
   if (!supports_video()) { return false; }
   var v = document.createElement("video");
-  return v.canPlayType('video/ogg; codecs="theora, vorbis"');
+  return v.canPlayType('video/webm; codecs="vp8"');
 }
