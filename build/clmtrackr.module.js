5331ff4b83c6cfb61edf536d777231959dd3406c
Audun Mathias Øygard <amoygard@gmail.com>
2017-08-06 21:56:03 +0200

    Rebuild

--- build/clmtrackr.module.js
+++ build/clmtrackr.module.js
@@ -14821,6 +14821,8 @@ var clm = {
 			first = true;
 			scoringHistory = [];
 			previousParameters = [];
+			currentPositions = [];
+			previousPositions = [];
 			for (var i = 0;i < currentParameters.length;i++) {
 				currentParameters[i] = 0;
 			}
